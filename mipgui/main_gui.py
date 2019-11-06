68dd565fb59ccbfdca61bd5304b81f7038ba6949
jradler-wassoc <jradler@wassoc.com>
2019-11-06 14:34:56 -0800

    Continued adding to the GUI for handling the file paths on different operating systems.
    Signed-off-by: jradler-wassoc <jradler@wassoc.com>

---
+++ mipgui/main_gui.py
@@ -0,0 +1,17 @@
+#!/bin/env python
+
+#  Copyright (c) 2019. Induced Polarization Associates, LLC, Seattle, WA
+
+# mipgui/main_gui.py
+"""
+Docstring
+#TODO: ALWAYS ALWAYS ALWAYS ADD A DOCSTRING! 
+"""
+import os
+import tkinter
+
+root = tkinter.Tk()
+
+
+
+if __name__ == "__main__":
